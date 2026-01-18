450ee5f7515154249c2aa29ada6b9df70254caed
Riya Patel <riyaptl3473@gmail.com>
2026-01-18 21:15:40 +0530

    Feat: Hazelnut added

--- src/pages/ShopsListPage.jsx
+++ src/pages/ShopsListPage.jsx
@@ -307,9 +307,9 @@ const ShopsListPage = () => {
         });
 
     const productsList = [
-        "Cranberry 50g", "Dryfruits 50g", "Peanuts 50g", "Mix seeds 50g", "Blueberry 50g",
+        "Cranberry 50g", "Dryfruits 50g", "Peanuts 50g", "Mix seeds 50g", "Blueberry 50g", "Hazelnut 50g",
         "Classic Coffee 50g", "Dark Coffee 50g", "Intense Coffee 50g", "Toxic Coffee 50g",
-        "Cranberry 25g", "Dryfruits 25g", "Peanuts 25g", "Mix seeds 25g", "Blueberry 25g",
+        "Cranberry 25g", "Dryfruits 25g", "Peanuts 25g", "Mix seeds 25g", "Blueberry 25g", "Hazelnut 25g",
         "Orange 25g", "Mint 25g", "Classic Coffee 25g", "Dark Coffee 25g",
         "Intense Coffee 25g", "Toxic Coffee 25g", "Gift box",
         "Hazelnut & Blueberries 55g", "Roasted Almonds & Pink Salt 55g", "Kiwi & Pineapple 55g", "Ginger & Cinnamon 55g", "Pistachio & Black Raisin 55g", "Dates & Raisin 55g"
